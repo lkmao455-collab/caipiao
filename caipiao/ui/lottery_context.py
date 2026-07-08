ac5bbff8beb323e21f10fc446b161e2588906ea4
lkmao455-collab <lkmao455@gmail.com>
2026-07-09 04:11:39 +0800

    cleanup: remove legacy generic ML strategy files and PDF artifacts
    - Delete caipiao/core/strategies/{ml,catboost,lightgbm,xgboost}_strategy.py
    
    - Update ui/lottery_context.py to use SSQXGBoostStrategy
    
    - Remove committed PDF build artifacts
    
    Closes Task 18

--- caipiao/ui/lottery_context.py
+++ caipiao/ui/lottery_context.py
@@ -20,7 +20,7 @@ from ..core.strategies.lotteries.ssq.balanced import SSQBalancedStrategy
 from ..core.strategies.advanced.lotteries.ssq.bayesian import SSQBayesianStrategy
 from ..core.strategies.advanced.lotteries.ssq.markov import SSQMarkovStrategy
 from ..core.strategies.lotteries.ssq.odd_even import SSQOddEvenStrategy
-from ..core.strategies.ml_strategy import MLStrategy
+from ..core.strategies.lotteries.ssq.ml.xgboost import SSQXGBoostStrategy
 from ..data.analyzer import DrawAnalyzer, LotteryAnalyzer
 from ..data.fetcher import LotteryDataFetcher
 from ..data.models import DrawRecord
@@ -68,7 +68,7 @@ class LotteryContext(QObject):
         if self.profile.key == "ssq":
             self.engine.register(SSQBalancedStrategy())
             self.engine.register(SSQOddEvenStrategy())
-            self.engine.register(MLStrategy("xgboost"))
+            self.engine.register(SSQXGBoostStrategy())
             self.engine.register(SSQBayesianStrategy())
             self.engine.register(SSQMarkovStrategy())
         elif self.profile.key == "3d":
